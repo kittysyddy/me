51dc38876bd28e2ee05d2311650e16b4334dc7c0
Sydney Sweeney <30902737+sydneysweeneydj@users.noreply.github.com>
2019-10-10 13:30:42 -0400

    Add more

--- .bash_profile
+++ .bash_profile
@@ -1,7 +1,7 @@
 PS1="\W 🖖 "
 git config --global user.email 30902737+sydneysweeneydj@users.noreply.github.com
 alias gs="git status"
-export PATH=$PATH:/Users/kittysyddy/Library/Python/2.7/bin
+export PATH=$PATH:$HOME/Library/Python/2.7/bin
 
 # note: subl cli should be readily available already because the installation path is already in $PATH
 # 🖖 which subl
--- initialization_script.sh
+++ initialization_script.sh
@@ -1,5 +1,17 @@
+cp config $HOME/.ssh/
+cp .bash_profile $HOME/
+source $HOME/.bash_profile
+cp .vimrc $HOME/
 brew cask install sublime-text
+brew install lastpass-cli
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py --user
 rm -rf get-pip.py
 pip install tokendito --user
+
+# need to automate this
+ssh-keygen -t rsa -b 4096 -C "30902737+sydneysweeneydj@users.noreply.github.com" -P ""
+
+eval "$(ssh-agent -s)"
+ssh-add -K $HOME/.ssh/id_rsa
+cat $HOME/.ssh/id_rsa.pub | pbcopy
