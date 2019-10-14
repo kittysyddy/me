b14194b4d8e62f341a59f050e42b83dca10615bf
Sydney Sweeney <30902737+sydneysweeneydj@users.noreply.github.com>
2019-10-14 10:26:33 -0400

    .

--- initialization_script.sh
+++ initialization_script.sh
@@ -8,6 +8,8 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py --user
 rm -rf get-pip.py
 pip install tokendito --user
+pip install awscli --user
+brew cask install amazon-music
 
 # need to automate this
 ssh-keygen -t rsa -b 4096 -C "30902737+sydneysweeneydj@users.noreply.github.com" -P ""
