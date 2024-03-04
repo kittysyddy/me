8801671661d34a1fdc2e4bb4801fea05df280392
syd <30902737+kittysyddy@users.noreply.github.com>
2024-03-04 10:56:08 -0500

    ⊂(•‿•)つ
    Made some changes for uniformity; brew over pip3 and some file writes
    
    ⊂(•‿•)つ
    
    Made some changes for uniformity; brew over pip3 and some file writes
    
    ⊂(•‿•)つ
    
    Made some changes for uniformity; brew over pip3 and some file writes

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Minimal dev env setup & config for new machines or across different machines.
 "I want to be able to checkout from GitHub, `subl` that repo with some themes and packages ready."  
 Will still need to do authentication to cloud providers after this ofc.
 
+
 ### ཐིཋྀ Run 
 
-1. `git clone https://github.com/kittysyddy/me.git me ; cd "$_"`
-1. `chmod +x runme.zsh; ./runme.zsh`
+1. `git clone https://github.com/kittysyddy/me.git me ; cd "$_"; chmod +x runme.zsh`
+1. `./runme.zsh`
+
 
 ### ❀ Installs and/or upgrades:
 
@@ -15,7 +17,6 @@ Will still need to do authentication to cloud providers after this ofc.
 * sublime
 * pip3
 * virtualenv
-* boto
 * cloudformation
 * git
 
@@ -27,8 +28,8 @@ Shell aliases in my .zshrc
 
 ### ✿ Personal Config
 
-* config/zprofile - Basics
 * config/zshrc - Shell customs
 * config/vimrc - Mostly for color
 * config/sublime - Themes & packages
+* config/art - my ascii art reference
 * git cli: generate new ssh key for github auth
--- runme.zsh
+++ runme.zsh
@@ -9,21 +9,18 @@
 # - Move shell config files & set shell vars
 # - Install homebrew
 # - Install Sublime & config for it
-# - python3 / pip3, virtualenv
-# - cloud dev: aws cli, boto3, cloudformation cli
+# - python3 / pip3
+# - cloud dev: virtualenv, aws cli, cloudformation cli
 # - git & ssh keys
 
-set -e
 
 #########
 # shell #
 #########
 
-cp ./art.txt $HOME/.art.txt # ascii art (｡•́‿•̀｡)
-cp ./config/zprofile $HOME/.zprofile
+cp ./config/art.txt $HOME/.art.txt # ascii art (｡•́‿•̀｡)
 cp ./config/zshrc $HOME/.zshrc
 cp ./config/vimrc $HOME/.vimrc
-source $HOME/.zprofile
 source $HOME/.zshrc
 
 
@@ -31,16 +28,18 @@ source $HOME/.zshrc
 # brew #
 ########
 
-# Install brew
+Install brew
 NONINTERACTIVE=1 /bin/bash -c \
-   "$(curl -fsSL \
-      https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+"$(curl -fsSL \
+   https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # Add to path
-eval "$(/opt/homebrew/bin/brew shellenv)"
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zprofile
+source $HOME/.zprofile
 
 # Just a peep
 which brew
 echo $PATH
+
 
 ###########
 # Sublime #
@@ -49,6 +48,8 @@ brew install --cask sublime-text
 which subl
 export DATA_DIR="$HOME/Library/Application Support/Sublime Text" # assuming MacOS
 cp -r ./config/Sublime\ Text $DATA_DIR 
+echo 'export PATH="$PATH:/Applications/Sublime Text.app/Contents/SharedSupport/bin"'  >> $HOME/.zprofile
+source $HOME/.zprofile
 
 
 ###########################
@@ -57,20 +58,16 @@ cp -r ./config/Sublime\ Text $DATA_DIR
 
 # pip3 was already installed
 # add py3 to path for the upcoming package installations to the local user
-export PATH="$PATH:$HOME/Library/Python/3.9/bin"
-# get virtualenv
-
-# Hygiene / update pip3, grab virtualenv & boto
-pip3 install --no-input --upgrade pip
-pip3 install --no-input virtualenv
+echo 'export PATH="$PATH:$HOME/Library/Python/3.9/bin"' >> $HOME/.zprofile
+source $HOME/.zprofile
 
 
-#############
-# cloud dev #
-#############
+#######
+# dev #
+#######
 
-pip3 install awscli
-pip3 install boto3
+brew install virtualenv
+brew install awscli
 brew install cloudformation-cli
 
 
@@ -80,20 +77,19 @@ brew install cloudformation-cli
 
 brew install git
 
-random_number=$((RANDOM % 100))
+random_number=$((RANDOM % 1000))
 github_key_lcn="$HOME/.ssh/github_main$random_number"
 github_email="30902737+kittysyddy@users.noreply.github.com"
 ssh-keygen -t ed25519 -f $github_key_lcn -N "" -C $github_email
+# add new key to gitconfig
+# file will be created if it does not exist; the double > with cat makes it append
 cat <<EOF >> $HOME/.gitconfig
 [user]
-    name = syd
-    email = $github_email
+  name = syd
+  email = $github_email
 EOF
 
-
-# add new key to ssh config.
-# file will be created if it does not exist
-# the double > with cat makes it append
+# add new key to ssh config
 cat <<EOF >> $HOME/.ssh/config
 
 Host github.com
@@ -109,3 +105,4 @@ github_key_pub=$github_key_lcn".pub"
 echo "Adding public key to clipboard & opening GitHub"
 open "https://github.com/settings/keys"
 cat $github_key_pub | pbcopy
+
